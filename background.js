aca9331d2868b06c313ba02642ddba99fa87bab1
rockito10 <107514701+rockito10@users.noreply.github.com>
2025-08-11 14:59:34 -0300

    Update background.js

--- background.js
+++ background.js
@@ -1,50 +1,79 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-chrome.runtime.onMessage.addListener(
-// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-(request, sender, sendResponse) => __awaiter(void 0, void 0, void 0, function* () {
-    if (request.message === "setTags") {
-        addTagsAndTitleOnCurrentTab(request.tags);
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
     }
-}));
-function addTagsAndTitleOnCurrentTab(tags) {
-    let n = 0;
-    chrome.storage.local.get("nextSketchId", (data) => {
-        n = data.nextSketchId;
-        const title = `Sketch #${n}`;
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            const tab = tabs[0];
-            if (!tab || tab.id === undefined)
-                return;
-            chrome.scripting.executeScript({
-                target: {
-                    tabId: tab.id,
-                },
-                func: addTagsOnDocument,
-                args: [tags, n], //tags and title
-            });
-        });
+    return new (P || (P = Promise))(function (resolve, reject) {
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
+  };
+chrome.runtime.onMessage.addListener(
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  (request, sender, sendResponse) =>
+    __awaiter(void 0, void 0, void 0, function* () {
+      if (request.message === "setTags") {
+        addTagsAndTitleOnCurrentTab(request.tags);
+      }
+    })
+);
+function addTagsAndTitleOnCurrentTab(tags) {
+  let n = 0;
+  chrome.storage.local.get("nextSketchId", (data) => {
+    // get the id of the next drawing
+    n = data.nextSketchId;
+    if (n == null)
+      console.error(
+        "nextSketchId missing, please set it before using the extension. Use 'chrome.storage.local.set({ nextSketchId: <NUMBER> });' "
+      );
+    if (n == null) return;
+
+    const title = `Sketch #${n}`;
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const tab = tabs[0];
+      if (!tab || tab.id === undefined) return;
+      chrome.scripting.executeScript({
+        target: {
+          tabId: tab.id,
+        },
+        func: addTagsOnDocument,
+        args: [tags, n], //tags and title
+      });
+    });
+  });
 }
 function addTagsOnDocument(tags, nextSketchId) {
-    const tagsInput = document.getElementsByClassName("_1H6JV")[3]; // DA input
-    // paste event
-    const pasteEvent = new ClipboardEvent("paste", {
-        bubbles: true,
-        cancelable: true,
-        clipboardData: new DataTransfer(), // For providing clipboard data
-    });
-    if (!pasteEvent.clipboardData)
-        return;
-    // tags and replace data of clipboard to them
-    pasteEvent.clipboardData.setData("text/plain", tags);
-    tagsInput.dispatchEvent(pasteEvent);
+  const tagsInput = document.getElementsByClassName("_1H6JV")[3]; // DA input
+  // paste event
+  const pasteEvent = new ClipboardEvent("paste", {
+    bubbles: true,
+    cancelable: true,
+    clipboardData: new DataTransfer(), // For providing clipboard data
+  });
+  if (!pasteEvent.clipboardData) return;
+  // tags and replace data of clipboard to them
+  pasteEvent.clipboardData.setData("text/plain", tags);
+  tagsInput.dispatchEvent(pasteEvent);
 }
